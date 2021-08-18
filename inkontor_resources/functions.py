bfcf2f83f41fcf1329c54f6bc18da0b71baedf30
Henrik Kiepe <hkiepe@inkontor.com>
2021-08-18 09:10:21 +0200

    Improved descriptions of the functions and added a special function to return only all parcel sku

--- inkontor_resources/functions.py
+++ inkontor_resources/functions.py
@@ -21,22 +21,33 @@ def check_sub_parcels(project_path, parcels_file, parcels_workbook):
     sku_workbook = openpyxl.load_workbook(parcels_file)
     sku_sheet = sku_workbook[parcels_workbook]
     products = []
-    parcels = []
-    # iterate the source and create an array with all sub parcels
+    # read the whole first column in products array because we need it later to check if sub parcels are in
+    # products array
     for i in range(3, sku_sheet.max_row + 1):
-        # read the whole first column in products array because we need it later to check if sub parcels are in
-        # products array
         if sku_sheet.cell(row=i, column=1).value not in products:
             products.append(sku_sheet.cell(row=i, column=1).value)
-        # here we create the sub parcel array
-        for j in range(12, 18):
-            if sku_sheet.cell(row=i, column=j).value is not None and sku_sheet.cell(row=i, column=j).value != '-':
-                if sku_sheet.cell(row=i, column=j).value not in parcels:
-                    parcels.append(sku_sheet.cell(row=i, column=j).value)
+    # here we create the sub parcel array
+    parcels = get_parcels_sku(project_path, parcels_file, parcels_workbook)
     # check if the sub parcels have a corresponding value in the product array. If not we raise an ValueError
     for parcel in parcels:
         if parcel not in products:
             raise ValueError(f'{parcel} has no values in source file')
+
+
+# get all parcel sku from the file
+def get_parcels_sku(project_path, parcels_file, parcels_workbook):
+    os.chdir(project_path)
+    sku_workbook = openpyxl.load_workbook(parcels_file)
+    sku_sheet = sku_workbook[parcels_workbook]
+    parcels_sku = []
+    for i in range(3, sku_sheet.max_row + 1):
+        for j in range(12, 18):
+            # check and filter out unusable characters
+            if sku_sheet.cell(row=i, column=j).value is not None and sku_sheet.cell(row=i, column=j).value != '-':
+                # add sku to array and check for duplicates
+                if sku_sheet.cell(row=i, column=j).value not in parcels_sku:
+                    parcels_sku.append(sku_sheet.cell(row=i, column=j).value)
+    return parcels_sku
 
 
 # Read in all products into objects from the source file and create a products array with all product objects.
@@ -45,9 +56,12 @@ def read_in_all_products(project_path, parcels_file, parcels_workbook):
     sku_workbook = openpyxl.load_workbook(parcels_file)
     sku_sheet = sku_workbook[parcels_workbook]
     products = []
+    parcels_sku = get_parcels_sku(project_path, parcels_file, parcels_workbook)
     # creates the products array with Products objects
     for i in range(3, sku_sheet.max_row + 1):
-        products.append(Product(sku_sheet.cell(row=i, column=1).value, sku_sheet.cell(row=i, column=2).value))
+        # filter out sub parcel sku
+        if sku_sheet.cell(row=i, column=1).value not in parcels_sku:
+            products.append(Product(sku_sheet.cell(row=i, column=1).value, sku_sheet.cell(row=i, column=2).value))
     return products
 
 
@@ -56,18 +70,11 @@ def read_in_all_parcels(project_path, parcels_file, parcels_workbook):
     os.chdir(project_path)
     sku_workbook = openpyxl.load_workbook(parcels_file)
     sku_sheet = sku_workbook[parcels_workbook]
-    parcels_sku = []
+    # get all parcel sku from the file
+    parcels_sku = get_parcels_sku(project_path, parcels_file, parcels_workbook)
     parcels = []
-    # First we grab all the sub parcels sku in one array. Then we iterate the array to create the sub parcels with
-    # values from source file
-    for i in range(3, sku_sheet.max_row + 1):
-        # here we create the parcels_sku array
-        for j in range(12, 18):
-            if sku_sheet.cell(row=i, column=j).value is not None and sku_sheet.cell(row=i, column=j).value != '-':
-                if sku_sheet.cell(row=i, column=j).value not in parcels_sku:
-                    parcels_sku.append(sku_sheet.cell(row=i, column=j).value)
-    # now we iterate through the source file and when we find an sku which is also in parcels_sku array we create the
-    # parcel object.
+    # Iterate through the file and if a sub parcel sku matches with the value in the parcels_sku array we create an
+    # instance of the Parcel object.
     for i in range(3, sku_sheet.max_row + 1):
         if sku_sheet.cell(row=i, column=1).value in parcels_sku:
             parcels.append(Parcel(sku_sheet.cell(row=i, column=1).value, sku_sheet.cell(row=i, column=2).value))
